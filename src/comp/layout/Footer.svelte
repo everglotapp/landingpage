d25fb8463950abc8c8e8e2cbf172b230b4bb1660
haveyaseen <haveyaread@gmail.com>
2021-02-08 20:00:15 +0100

    wrap footer on mobile

--- src/comp/layout/Footer.svelte
+++ src/comp/layout/Footer.svelte
@@ -1,8 +1,8 @@
-<footer class="container flex flex-column justify-between mx-auto py-4 md:py-8 px-8 text-gray-bitdark">
-    <div class="flex space-x-8 justify-start">
+<footer class="container flex flex-col-reverse sm:flex-row sm:justify-between mx-auto py-4 md:py-8 px-8 text-gray-bitdark">
+    <div class="flex self-center sm:space-x-8 sm:justify-start flex-col sm:flex-row">
         <span>&copy; 2021 Everglot.</span>
     </div>
-    <div class="flex space-x-8 justify-end">
+    <div class="flex self-center sm:space-x-8 sm:justify-end flex-col sm:flex-row">
         <span>Privacy</span>
         <span>Imprint</span>
     </div>
