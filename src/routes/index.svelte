e60fdc33dbd72956da5badd8d68ef12527105640
haveyaseen <haveyaread@gmail.com>
2021-02-08 22:18:01 +0100

    remove "it's free"

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -80,7 +80,7 @@
 
 <section id="footer-cta" class="bg-primary-lightest shadow-sm relative" style="--footer-cta-bg:url('')">
     <div class="flex flex-col md:flex-row md:items-center space-y-8 md:space-y-0 md:space-x-8 container mx-auto px-4 md:px-8 py-8 sm:py-12 md:py-24 relative">
-        <p class="font-bold">Sign up, it's free!</p>
+        <p class="font-bold">Sign up right away!</p>
         <ButtonLarge
             href="join"
             className="w-full justify-center md:w-auto mb-3 md:mb-0 md:mr-1"
