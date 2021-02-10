32db17ce8285f78911b8dafad3c17192eeaeb63f
haveyaseen <haveyaread@gmail.com>
2021-02-10 22:36:42 +0100

    add basic styling to error page

--- src/routes/_error.svelte
+++ src/routes/_error.svelte
@@ -1,40 +1,44 @@
 <script lang="ts">
-	export let status: number;
-	export let error: Error;
+    import PageTitle from "../comp/typography/PageTitle.svelte"
+    export let status: number
+    export let error: Error
 
-	const dev = process.env.NODE_ENV === 'development';
+    const dev = process.env.NODE_ENV === "development"
 </script>
 
-<style>
-	h1, p {
-		margin: 0 auto;
-	}
-
-	h1 {
-		font-size: 2.8em;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
-
-	p {
-		margin: 1em auto;
-	}
-
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
-	}
-</style>
-
 <svelte:head>
-	<title>{status}</title>
+    <title>{status}</title>
 </svelte:head>
 
-<h1>{status}</h1>
+<div class="container px-4 mx-auto mt-16 mb-32 max-w-sm">
+    <PageTitle>{status}</PageTitle>
 
-<p>{error.message}</p>
+    <p>{error.message}</p>
 
-{#if dev && error.stack}
-	<pre>{error.stack}</pre>
-{/if}
+    {#if dev && error.stack}
+        <pre>{error.stack}</pre>
+    {/if}
+</div>
+
+<style>
+    h1,
+    p {
+        margin: 0 auto;
+    }
+
+    h1 {
+        font-size: 2.8em;
+        font-weight: 700;
+        margin: 0 0 0.5em 0;
+    }
+
+    p {
+        margin: 1em auto;
+    }
+
+    @media (min-width: 480px) {
+        h1 {
+            font-size: 4em;
+        }
+    }
+</style>
