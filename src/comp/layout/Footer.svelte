4bdf320c3557d350266c4a9464e10aa6c01d76bf
haveyaseen <haveyaread@gmail.com>
2021-07-01 16:28:23 +0200

    add squirrel image attribution to footer

--- src/comp/layout/Footer.svelte
+++ src/comp/layout/Footer.svelte
@@ -3,13 +3,25 @@
 </script>
 
 <footer
-    class="container flex flex-col-reverse sm:flex-row sm:justify-between mx-auto py-4 md:py-8 px-8 text-gray-lightest"
+    class="container flex flex-col-reverse sm:flex-row sm:justify-between mx-auto py-4 md:py-8 px-8 space-y-2 md:space-y-0 text-gray-lightest"
     class:index={typeof segment === undefined}
 >
     <div
-        class="flex self-center sm:space-x-8 sm:justify-start flex-col sm:flex-row"
+        class="flex self-center sm:space-x-1 sm:justify-start flex-col sm:flex-row"
     >
-        <span>&copy; 2021 Everglot.</span>
+        <span>&copy; 2021 Everglot.</span><span class="text-gray-bitlight"
+            >Squirrel image made by <a
+                href="https://www.flaticon.com/authors/smashicons"
+                title="Smashicons"
+                class="font-normal">Smashicons</a
+            >
+            from
+            <a
+                href="https://www.flaticon.com/"
+                title="Flaticon"
+                class="font-normal">www.flaticon.com</a
+            >.</span
+        >
     </div>
     <div
         class="flex self-center sm:space-x-8 sm:justify-end flex-col sm:flex-row"
