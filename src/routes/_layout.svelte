2a7f80eb5cd8779927fd03cd0f901d47dd8a1aec
haveyaseen <haveyaread@gmail.com>
2021-12-06 14:27:15 +0100

    add tracking pixel fallback, fix server side rendering

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,19 +1,21 @@
 <script context="module" lang="ts">
-    const _paq = (window._paq = window._paq || [])
-    /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-    _paq.push(["trackPageView"])
-    _paq.push(["enableLinkTracking"])
-    ;(function () {
-        const u = "//internal-analytics.everglot.com/"
-        _paq.push(["setTrackerUrl", u + "matomo.php"])
-        _paq.push(["setSiteId", "1"])
-        const d = document
-        const g = d.createElement("script")
-        const s = d.getElementsByTagName("script")[0]
-        g.async = true
-        g.src = u + "matomo.js"
-        s.parentNode!.insertBefore(g, s)
-    })()
+    if (typeof window !== "undefined") {
+        const _paq = (window._paq = window._paq || [])
+        /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+        _paq.push(["trackPageView"])
+        _paq.push(["enableLinkTracking"])
+        ;(function () {
+            const u = "//internal-analytics.everglot.com/"
+            _paq.push(["setTrackerUrl", u + "matomo.php"])
+            _paq.push(["setSiteId", "1"])
+            const d = document
+            const g = d.createElement("script")
+            const s = d.getElementsByTagName("script")[0]
+            g.async = true
+            g.src = u + "matomo.js"
+            s.parentNode!.insertBefore(g, s)
+        })()
+    }
 </script>
 
 <script lang="ts">
@@ -34,6 +36,15 @@
     // @ts-ignore
     $: segment, change()
 </script>
+
+<noscript>
+    <img
+        referrerpolicy="no-referrer-when-downgrade"
+        src="https://internal-analytics.everglot.com/matomo.php?idsite=1&amp;rec=1"
+        style="border:0"
+        alt=""
+    />
+</noscript>
 
 <LocaleProvider {segment}>
     <MainNav {segment} />
